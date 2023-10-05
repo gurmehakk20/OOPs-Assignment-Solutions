10cff22bb7e45bac892763bb9ce98bbdaeb9c71f
gurmehakk20 <125135241+gurmehakk20@users.noreply.github.com>
2023-10-05 09:22:30 +0530

    Update q21.cpp

--- q21.cpp
+++ q21.cpp
@@ -11,9 +11,12 @@ int main(){
         }
     }
 
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            cout << arr[i][j] << " ";
-        }
+    for (int i = 0; i < m; ){
+        for (int j = 0; j < n && i < m; j++){
+            cout << arr[i][j] << ", ";
+        } i++;
+        for (int j = n-1; j >= 0 && i < m; j--){
+            cout << arr[i][j] << ", ";
+        } i++;
     } cout << "END" ;
 }
